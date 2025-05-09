6574b11cf3905e592303bc3fe6b43963b2d9ee96
Pascal <pas111cal3@gmail.com>
2025-05-09 04:25:42 +0100

    product mobile  header completed anded some info to docs started personal account css

---
+++ Open-cart/scripts/data/products.js
@@ -0,0 +1,32 @@
+// using this file for all products
+//top deals(recommended products)
+
+
+const recommendedProducts = [
+  {img:'img', name: 'name' , price: 0, sellersIMg: 'img', sellersName: 'name', },
+  {img:'img', name: 'name' , price: 0, sellersIMg: 'img', sellersName: 'name', },
+  {img:'img', name: 'name' , price: 0, sellersIMg: 'img', sellersName: 'name', },
+  {img:'img', name: 'name' , price: 0, sellersIMg: 'img', sellersName: 'name', },
+  {img:'img', name: 'name' , price: 0, sellersIMg: 'img', sellersName: 'name', },
+  {img:'img', name: 'name' , price: 0, sellersIMg: 'img', sellersName: 'name', },
+  {img:'img', name: 'name' , price: 0, sellersIMg: 'img', sellersName: 'name', },
+  {img:'img', name: 'name' , price: 0, sellersIMg: 'img', sellersName: 'name', },
+  {img:'img', name: 'name' , price: 0, sellersIMg: 'img', sellersName: 'name', },
+  {img:'img', name: 'name' , price: 0, sellersIMg: 'img', sellersName: 'name', },
+];
+
+//rest of my products
+ export const products = [
+  {img:'img', name: 'name' , price: 0, sellersIMg: 'img', sellersName: 'name', },
+  {img:'img', name: 'name' , price: 0, sellersIMg: 'img', sellersName: 'name', },
+  {img:'img', name: 'name' , price: 0, sellersIMg: 'img', sellersName: 'name', },
+  {img:'img', name: 'name' , price: 0, sellersIMg: 'img', sellersName: 'name', },
+  {img:'img', name: 'name' , price: 0, sellersIMg: 'img', sellersName: 'name', },
+  {img:'img', name: 'name' , price: 0, sellersIMg: 'img', sellersName: 'name', },
+  {img:'img', name: 'name' , price: 0, sellersIMg: 'img', sellersName: 'name', },
+  {img:'img', name: 'name' , price: 0, sellersIMg: 'img', sellersName: 'name', },
+  {img:'img', name: 'name' , price: 0, sellersIMg: 'img', sellersName: 'name', },
+  {img:'img', name: 'name' , price: 0, sellersIMg: 'img', sellersName: 'name', },
+];
+console.log(products)
+
---
+++ Open-cart/scripts/scripts.js
@@ -0,0 +1,37 @@
+   import  {products}  from './data/products.js';
+
+
+  //  funtion below loops through the products array
+  // then generates the html for each product
+  let productsHTML = '';
+  function displayProducts() {
+     products.forEach((product) => {
+        productsHTML += `
+        <div class="product-card">
+           <!-- img div -->
+            <div  class="product-img-contaner bd-radius">
+              <img class="bd-radius" src="/Open-cart/media/productImages/headphone.jpg" alt="product-img">
+            </div>
+
+            <div>
+              <p class="product-name ">${product.name}</p>
+              <p class="product-price FWB">${product.price}</p>
+              <div class="seller-container">
+                <div class="sellers-img-container">
+                  <img  src="" alt="sellars img">
+                </div>
+                <p class="sellars-name">${product.sellersName}</p>
+              </div>
+            </div>
+              
+            <button>
+              Add To Cart
+            </button>
+        </div>
+        `
+     })
+    }
+     displayProducts();
+    document.querySelector('.product-container').innerHTML = productsHTML;
+
+ 
