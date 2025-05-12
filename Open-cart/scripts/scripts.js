f6ec815ba79ae65d5d346fc9aa553ce57c4a51f1
Pascal <pas111cal3@gmail.com>
2025-05-12 13:20:26 +0100

    site is responsive,only input mobile edith remains

--- Open-cart/scripts/scripts.js
+++ Open-cart/scripts/scripts.js
@@ -10,7 +10,7 @@
         <div class="product-card">
            <!-- img div -->
             <div  class="product-img-contaner bd-radius">
-              <img class="bd-radius" src=${product.img} alt="product-img">
+              <img loading="lazy" class="bd-radius" src=${product.img} alt="product-img">
             </div>
 
             <div>
@@ -18,7 +18,7 @@
               <p class="product-price FWB">$${product.price}</p>
               <div class="seller-container">
                 <div class="sellers-img-container">
-                  <img  src=${product.sellersIMg} alt="sellars img">
+                  <img loading="lazy" src=${product.sellersIMg} alt="sellars img">
                 </div>
                 <p class="sellars-name">${product.sellersName}</p>
               </div>
@@ -34,4 +34,33 @@
      displayProducts();
     document.querySelector('.product-container').innerHTML = productsHTML;
 
+
+
+    // script for responsive mobile side-bar
  
+const SidebarBtn = document.querySelector(".sidebar-btn");
+SidebarBtn.addEventListener("click",  ()=>{
+  let closeIcon = document.querySelector(".close-icon ");
+  let showIcon = document.querySelector(".show-icon ");
+  let sidebar = document.querySelector(".sidebar");
+  let sidebarLabel = sidebar.getAttribute('aria-label');
+  console.log(sidebar)
+
+  if (sidebarLabel === "sidebar closed") {
+    sidebar.setAttribute('aria-label', 'sidebar opened');
+    showIcon.classList.add("hide-icon");
+    closeIcon.classList.remove("hide-icon");
+
+    sidebar.classList.add("show-sidebar");
+
+    console.log(sidebar.getAttribute('aria-label'));
+  }else{
+    sidebar.setAttribute('aria-label', 'sidebar closed');
+    
+    sidebar.classList.remove("show-sidebar");
+console.log(sidebar.getAttribute('aria-label'));
+    closeIcon.classList.add("hide-icon");
+    showIcon.classList.remove("hide-icon");
+  }
+
+});
